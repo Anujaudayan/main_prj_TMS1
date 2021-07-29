7d8e69e92d16eb445c3dc195b1c8b692f2d45b64
mridulavijay <mridula.reghu@gmail.com>
2021-07-29 22:03:19 +0530

    calender changes

--- Frontend/src/app/calender/calender.component.ts
+++ Frontend/src/app/calender/calender.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarView } from 'angular-calendar';
+import { CalendarEvent } from 'calendar-utils';
+
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
@@ -10,11 +12,23 @@ export class CalenderComponent implements OnInit {
   
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
+  events: CalendarEvent[] = [
+    {
+      start: new Date('2021-07-30'),
+      title: 'An event with no end date',
+
+    }
+  ]
+  
   
   constructor() { }
 
   ngOnInit(): void {
     
+  }
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    alert('Event name:'+events[0].title+',Time:11:00 AM,Venue:Iris conference hall')
+    //this.openAppointmentList(date)
   }
  
 }
